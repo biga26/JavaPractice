909fee9b1fd0d589ef6ea2ff2450da3d3379e642
biga26 <begench.amanmyradov@gmail.com>
2024-03-30 03:42:47 -0400

    New

--- src/day01_hello_world/Main.java
+++ src/day01_hello_world/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
     // write your code here
         System.out.println("Hello World");
+        System.out.println("Biga uploaded");
     }
 
 
